b737222eaee3ebf18be48cf76067a49976b05ece
willyhp123 <limwilly0@gmail.com>
2022-04-25 15:45:08 +0700

    Revisi

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const Productrouter = require("./app/products/route");
-const Productrouterv2 = require("./app/product-v2/route");
+//const Productrouterv2 = require("./app/product-v2/route");
 // const log = require("./middleware/logger");
 const logger = require("morgan");
 const path = require("path");
@@ -9,7 +9,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(Productrouter);
-app.use(Productrouterv2);
+//app.use(Productrouterv2);
 app.use('/public',express.static(path.join(__dirname, "uploads")));
 app.use((req, res, next) => {
   res.status(404);
